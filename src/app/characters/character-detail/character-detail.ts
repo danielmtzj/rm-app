2d26156f9ac07206e6c98b69369dcdaddc230ad3
danielmtzj <danielmtzj.dev@gmail.com>
2025-09-26 16:07:25 -0600

    separate component for character details

--- src/app/characters/character-detail/character-detail.ts
+++ src/app/characters/character-detail/character-detail.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CharactersService } from '../services/characters.service';
 
 @Component({
   selector: 'app-character-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './character-detail.html',
   styleUrl: './character-detail.css'
 })
 export class CharacterDetail {
+  private route = inject(ActivatedRoute);
+  private service = inject(CharactersService);
+  protected readonly character = signal<any | null>(null);
 
+  constructor() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.service.getCharacter(id).then(data => this.character.set(data));
+  }
 }
