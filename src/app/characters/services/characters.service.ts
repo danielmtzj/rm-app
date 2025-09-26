839c63d1a96127c51e0a96c5c8de008d354c4008
danielmtzj <danielmtzj.dev@gmail.com>
2025-09-26 16:11:09 -0600

    implementation of the API in the character service

--- src/app/characters/services/characters.service.ts
+++ src/app/characters/services/characters.service.ts
@@ -4,5 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CharactersService {
-  
+  async getCharacters(): Promise<any[]> {
+    const res = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await res.json();
+    return data.results;
+  }
+  async getCharacter(id: number): Promise<any> {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    return await res.json();
+  }
 }
