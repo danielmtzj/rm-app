284d0cccfd1244f404f9d4ecb2f80fde0793b565
danielmtzj <danielmtzj.dev@gmail.com>
2025-09-26 17:57:38 -0600

    adjust the configuration and main input of the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter([
+      { path: '', redirectTo: 'characters', pathMatch: 'full' },
+      {
+        path: 'characters',
+        loadComponent: () =>
+          import('../app/characters/character-list/character-list').then(m => m.CharacterList)
+      },
+      {
+        path: 'characters/:id',
+        loadComponent: () =>
+          import('../app/characters/character-detail/character-detail').then(m => m.CharacterDetail)
+      },
+      {
+        path: 'favorites',
+        loadComponent: () =>
+          import('../app/favorites/favorite-list/favorite-list').then(m => m.FavoriteList)
+      }
+    ]),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };
